6c65c3b57310e98b169ef2c5e1aac16f1cf50ab9
Juan Jimenez <bozz212@gmail.com>
2023-02-01 08:56:40 -0800

    1st commit

---
+++ Week 2-1/functions-libraries/ball.js
@@ -0,0 +1,18 @@
+var ball = create()
+size(ball, 300, 300)
+color(ball, 255, 0, 0)
+move(ball, -300, -300)
+colorRandom(ball)
+
+/* var counter=0;
+while (counter<10) {
+    create();
+    counter + 1;
+} */
+
+ var list = []; 
+
+list.push(create());
+list.push(create());
+list.push(create());
+list.push(create());
---
+++ Week 2-1/functions-objects/ball.js
@@ -0,0 +1,9 @@
+var length = data.length;
+
+// you work goes here
+// -----------------------
+data[400]
+ball.x
+ball.y
+ball.color
+create(ball.x, ball.y, ball.color)
